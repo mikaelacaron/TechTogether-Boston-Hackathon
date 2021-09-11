abd66e59aaf04dcd397642e7eec7e7e46bf7ce72
Mikaela Caron <mikaelacaron@gmail.com>
2021-09-11 17:41:01 -0400

    Initial Commit

---
+++ TechTogether-Boston-Hackathon/TechTogether_Boston_HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechTogether_Boston_HackathonApp.swift
+//  TechTogether-Boston-Hackathon
+//
+//  Created by Mikaela Caron on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TechTogether_Boston_HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
