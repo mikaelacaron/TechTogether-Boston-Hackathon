77812eaf9cabc735437a33c232e3b629300520c8
Mikaela Caron <mikaelacaron@gmail.com>
2021-09-11 18:47:49 -0400

    Demo is done!

--- TechTogether-Boston-Hackathon/ContentView.swift
+++ TechTogether-Boston-Hackathon/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var predictions = [
+        "Yes", "No", "It Will Pass",
+        "Count on It", "Maybe", "You're Hot",
+        "Ask Again", "No Doubt", "Absolutely",
+        "Go For It", "Wait For It", "Not Now",
+        "Cannot Tell Now", "Very Likely", "It is OK"
+    ]
+    
+    @State private var randomNumber = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button {
+            self.randomNumber = Int.random(in: 0..<predictions.count)
+        } label: {
+            Text("\(predictions[self.randomNumber])")
+                .bold()
+                .font(.title)
+                .foregroundColor(Color.white)
+                .frame(width: 300, height: 300)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.black, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                .clipShape(Circle())
+        }
     }
 }
 
